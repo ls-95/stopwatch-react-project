65c700c74d83b97030fcfed64048c7937e10a982
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-07-04 12:01:54 +0200

    Add Stopwatch styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Stopwatch from "./components/Stopwatch";
+import "./styles/Stopwatch.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Stopwatch />
+    </div>
+  );
 }
 
 export default App;
